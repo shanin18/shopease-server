4cfdfdf7cf4c82ae092496cf0343af5d0bda61f2
shanin18 <shaninh12@gmail.com>
2024-05-31 18:41:24 +0600

    made endpoints

---
+++ routes/updateProduct.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { ObjectId } = require("mongodb");
+
+const router = express.Router();
+
+const updateProduct = (database) => {
+  const productsCollection = database.collection("products");
+
+  router.put("/:id", async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const updatedProductData = req.body;
+
+      // Validate input data
+      if (!ObjectId.isValid(productId)) {
+        return res.status(400).json({ message: "Invalid product ID" });
+      }
+
+      // Update data
+      const filter = { _id: new ObjectId(productId) };
+      const updateData = {
+        $set: {
+          name: updatedProductData.name,
+          seller: updatedProductData.seller,
+          price: updatedProductData.price,
+          ratings: updatedProductData.ratings,
+          img: updatedProductData.img,
+          stock: updatedProductData.stock,
+          shipping: updatedProductData.shipping,
+          ratingsCount: updatedProductData.ratingsCount,
+          quantity: updatedProductData.quantity,
+        },
+      };
+
+      const result = await productsCollection.updateOne(filter, updateData);
+
+      if (result.modifiedCount === 0) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+
+      res.json({ message: "Product updated successfully" });
+    } catch (err) {
+      console.error("Error updating product:", err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+
+  return router;
+};
+
+module.exports = updateProduct;
---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const getProducts = require("./routes/getProducts");
+const getProductDetailsById = require("./routes/getProductDetailsById");
+const addProduct = require("./routes/addProduct");
+const updateProduct = require("./routes/updateProduct");
+const deleteProduct = require("./routes/deleteProduct");
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+// Initialize Express App
+const app = express();
+
+// Middleware
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+};
+
+// Enable CORS
+app.use(cors(corsConfig));
+app.use(express.json());
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.gacal02.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("shopease");
+
+    // Routes
+    app.use("/products", getProducts(database));
+    app.use("/products", getProductDetailsById(database));
+    app.use("/add-product", addProduct(database));
+    app.use("/update-product", updateProduct(database));
+    app.use("/delete-product", deleteProduct(database));
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to shopease server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
